b6bc96a2634ee74a7832cad95e7b43ede2faf055
OCHE Samson <s.oche@outlook.com>
2026-04-29 12:47:32 +0100

    adding id in excel

--- pages/views.py
+++ pages/views.py
@@ -49,7 +49,7 @@ def import_excel_data(request):
 
         for data in import_data:
             value = Sales(
-                data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]
+                data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8]
             )
             value.save()
     return render(request, 'dashboard/dataform.html', {})
