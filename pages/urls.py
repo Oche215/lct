87dcb92d2f824751d3f276bffe0430a2a12d3399
OCHE Samson <s.oche@outlook.com>
2026-05-04 21:56:35 +0100

    view chart4

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import home, chart_data, chart_view, chart_page, import_data
 
 urlpatterns = [
-    path('import/', import_data, name='inventory'),
+    path('import/', import_data, name='import'),
     path("view-chart/", chart_page, name="view_chart"),
     path('chart-data/', chart_data, name='chart_data'),
     path('chart-view/', chart_view, name='chart_view'),
