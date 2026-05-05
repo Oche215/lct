6f568759b8286cbce06c8ac22a58710fe2448696
OCHE Samson <s.oche@outlook.com>
2026-05-05 11:50:09 +0100

    chart 7

--- pages/views.py
+++ pages/views.py
@@ -136,8 +136,9 @@ def _process_sample_sales(imported_data):
 
 
 def chart_page(request):
+    chart1 = sales_chart_data(request)
 
-    return render(request, 'dashboard/chart.html', {})
+    return render(request, 'dashboard/chart.html', {'chart1': chart1})
 
 def sales_chart_data(request):
     # Fetch data from database
