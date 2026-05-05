63d48b8d3245132d5d6d9fe810cf70eee1fd5ebb
OCHE Samson <s.oche@outlook.com>
2026-05-05 09:47:37 +0100

    chart4

--- pages/urls.py
+++ pages/urls.py
@@ -3,7 +3,7 @@ from .views import home, chart_data, chart_view, chart_page, import_data
 
 urlpatterns = [
     path('import/', import_data, name='import'),
-    path("view-chart/", chart_page, name="view_chart"),
+    path("chart-page/", chart_page, name="chart-page"),
     path('chart-data/', chart_data, name='chart_data'),
     path('chart-view/', chart_view, name='chart_view'),
     path("", home, name="home"),
