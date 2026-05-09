40694fc27cb728730a88d4291473b474dff2d1fc
OCHE Samson <s.oche@outlook.com>
2026-05-09 23:40:08 +0100

    wine chart4

--- static/dashboard/assets/demo/wineChart.js
+++ static/dashboard/assets/demo/wineChart.js
@@ -2,7 +2,7 @@ const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
 const yValues = [55, 49, 44, 24, 15];
 const barColors = ["red", "green","blue","orange","brown"];
 
-const ctx = document.getElementById('wineChart');
+const ctx = document.getElementById('wineChart').getContext('2d');
 
 new Chart(ctx, {
   type: "bar",
